83e5fedecb6bcd1a37c77c3e9dde70e8baa7dc78
Huberth <vladimirhuberth@hotmail.com>
2021-09-09 11:25:24 -0300

    feature: get local storage functions

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,8 +1,11 @@
 export const setListPokemonAlreadyPlayed = (list: number[]) => localStorage.setItem('ALREADY_PLAYED', `${list}`)
+export const getPokemonAlreadyPlayed = localStorage.getItem('ALREADY_PLAYED')
 export const deleteListPokemonAlreadyPlayed = () => localStorage.removeItem('ALREADY_PLAYED')
 
 export const setScore = (list: number[]) => localStorage.setItem('SCORE', `${list}`)
+export const getScore = localStorage.getItem('SCORE')
 export const deleteScore = () => localStorage.removeItem('SCORE')
 
 export const setGeneration = (gen: number) => localStorage.setItem('GENERATION', `${gen}`)
+export const getGeneration = localStorage.getItem('GENERATION')
 export const deleteGeneration = () => localStorage.removeItem('GENERATION')
